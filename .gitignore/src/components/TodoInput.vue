a183024f5cdef5285b74ba3a0aee8abb5f224d3b
dnjsuql <68069455+dnjsuql@users.noreply.github.com>
2021-06-03 17:57:02 +0900

    Add files via upload

---
+++ .gitignore/src/components/TodoInput.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" @keyup.enter="addTodo">
+    <span class="addContainer" @click="addTodo">
+      <i class="fas fa-plus addBtn"></i>
+    </span>
+
+    <!-- <button id="show-modal" @click="showModal = true">Show Modal</button> -->
+    <Modal v-if="showModal" @close="showModal = false">
+      <h3 slot="header">
+        경고!
+        <i class="closeModalBtn fas fa-times" @click="showModal = false"></i>
+      </h3>
+      <p slot="body">
+        무언가를 입력하세요
+      </p>
+    </Modal>
+ </div>
+</template>
+
+<script>
+
+import Modal from './common/Modal.vue'
+
+export default {
+  data() {
+    return {
+      newTodoItem: '',
+      showModal: false
+    }
+  },
+  methods: {
+    addTodo() {
+      if(this.newTodoItem !== '') {
+        this.$store.commit('addOneItem', this.newTodoItem);
+        this.clearInput();
+      } else {
+        this.showModal = !this.showModal;
+      }
+    },
+    clearInput() {
+      this.newTodoItem = '';
+    }
+  },
+  components: {
+    Modal
+  }
+}
+</script>
+
+<style scoped>
+  input:focus {
+    outline: none;
+  }
+  .inputBox {
+    background-color: #fff;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 5px;
+  }
+  .inputBox input {
+    border-style: none;
+    font-size: 0.9rem;
+  }
+  .addContainer {
+    float: right;
+    background: linear-gradient(to right, #6478FB, #8763FB);
+    display: block;
+    width: 3rem;
+    border-radius: 0 5px 5px 0;
+  }
+  .addBtn {
+    color: #fff;
+    vertical-align: middle;
+  }
+  .closeModalBtn {
+    color: #42b983;
+  }
+</style>
